d15c4ffeb1b484dde2b2f7ddd094106cd06b5cde
nicholasages <nicholasy34@nycstudents.net>
2024-09-30 11:06:31 -0400

    something something
    asdfghjkl

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed!';
+});
+
+document.querySelector('button').addEventListener('click', function() {
+    document.querySelector('p').innerHTML =
+    'Class Riggor blbibubjubjfubjufbububfbfubfdubdfbdfblfblfblfblfblf';
+});
